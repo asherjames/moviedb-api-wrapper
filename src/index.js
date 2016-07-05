54f815709a32d72fd9e26d1ba6025221666b5cf9
asherjames <asherjames.nz@gmail.com>
2016-07-05 23:03:15 +1200

    Add cors middleware

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 const genreRoute = require('./router')
 
 const app = express()
 
+app.use(cors({credentials: true, origin: true}))
+
 app.use('/filim-api', genreRoute)
+
 
 app.listen(3000, () => {
 	console.log("Filim ready for film-finding on 3000...")
