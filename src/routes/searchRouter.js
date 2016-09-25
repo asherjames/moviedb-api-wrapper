e61e1bbf496ffa080ef744c52270b60ab6c1b13b
asherjames <asherjames.nz@gmail.com>
2016-09-25 13:07:41 +1300

    Add actor search endpoint

--- src/routes/searchRouter.js
+++ src/routes/searchRouter.js
@@ -10,12 +10,22 @@ searchRoute.get('/search/movie', (req, res) => {
         "release_date.gte": req.query.toReleaseYear + "-01-01",
         "release_date.lte": req.query.fromReleaseYear + "-01-01"
     }
-
     net.getWithParams(conf.discoverMoviesUrl, reqObj, data => res.json(data))
 })
 
-searchRoute.get('search/tv', (req, res) => {
+searchRoute.get('/search/tv', (req, res) => {
     //TODO
+})
+
+searchRoute.get('/search/actor', (req, res) => {
+    let reqObj = {
+        "query": req.query.searchTerm,
+        "include_adult": false
+    }
+    net.getWithParams(conf.searchActorsUrl, reqObj, data => {
+        let actorArray = data.results.map( a => ({name: a.name, id: a.id}))
+        res.json(actorArray)
+    })
 })
 
 module.exports = searchRoute
